dab88c707321d7a5c89569064503f2dc1d43e102
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2023-05-15 19:04:21 +1000

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+let SerialLogger: MicroBitLoggers.SerialLogger.SerialLogger = MicroBitLoggers.SerialLogger.createSerialLogger()
+let DataLogger: MicroBitLoggers.DataLogger.DataLogger = MicroBitLoggers.DataLogger.createDataLogger()
+let loggers = [SerialLogger, DataLogger]
+
+let step = 0
+
 basic.forever(function () {
-	
+	step++
+    loggers.forEach((value: MicroBitLogger.ILogger, index) => {
+        value.log(MicroBitLogger.LogType.Info, MicroBitLogger.generateSource(["main", "forever"]), `At cycle ${step}.`)
+    })
 })
